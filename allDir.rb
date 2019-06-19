82e3bca77a90d0cd2b5169d904e245b33a465b29
Govind Sanap <gsanap@tavisca.in>
2019-06-19 19:36:27 +0530

    added method and user input

--- allDir.rb
+++ allDir.rb
@@ -1,3 +1,8 @@
-Dir.chdir(".")
-subdir_list=Dir["*"].reject{|o| not File.directory?(o)}
-puts subdir_list;
+puts "Enter the name of directory or . for current directory"
+path = gets.chomp
+def load(path1)
+  	Dir.chdir(path1)
+	subdir_list=Dir["*"].reject{|o| not File.directory?(o)}
+	puts subdir_list
+end
+load(path)	
